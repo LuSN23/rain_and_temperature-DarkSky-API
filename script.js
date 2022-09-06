1b8674e3192a79e9ee6f5872bad2c50e1d78d5c8
Luciana Sarai Nunes <lucianasarainunes@gmail.com>
2022-09-06 20:21:08 -0300

    preparate the request and initialize variables

---
+++ script.js
@@ -0,0 +1,25 @@
+let uri = './sample-weather.json'; //JSON sample(simulation of an API)
+let req = new Request(uri, {method: 'GET'}); //Request instance
+let container, df; //Initializing variables of container and the document fragment
+
+document.addEventListener('DOMContentLoaded', init); //Wait to the page load then trigger the callback function init
+
+function init(){
+    container = document.getElementById('container'); //take the div with the container id
+    df = new DocumentFragment(); //DocumentFragment instance
+
+    fetch(req)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }else{
+                throw new Error('Bad HTTP!');
+            }
+        })
+        .then(data => {
+
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
+}
